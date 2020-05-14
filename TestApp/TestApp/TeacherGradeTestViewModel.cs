7c43ef9af049afe00b98dde06915d8cf9ff695a2
mikaelollhage <mikaelollhage@gmail.com>
2020-05-14 14:19:41 +0200

    TeacherGradeTestViewModel Class created. More regions added to Question Class

---
+++ TestApp/TestApp/TeacherGradeTestViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class TeacherGradeTestViewModel
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        private static TeacherGradeTestViewModel instance = null;
+        private static readonly object padlock = new object();
+        #endregion
+
+        #region Constructors
+        public TeacherGradeTestViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public static TeacherGradeTestViewModel Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance==null)
+                    {
+                        instance = new TeacherGradeTestViewModel();
+                    }
+                    return instance;
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void GradeTextQuestion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUngradedTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUngradedQuestion()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+    }
+}
