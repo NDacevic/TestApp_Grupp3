a7592ceff201dec1628ad2af1846eed4d7829072
NDacevic <n.dacevic@gmail.com>
2020-05-15 15:02:48 +0200

    Created initial functionality in the codebehind and added some buttons and textblocks to the CreateQuestionView

--- TestApp/TestApp/View/Teacher/CreateQuestionView.xaml.cs
+++ TestApp/TestApp/View/Teacher/CreateQuestionView.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TestApp.Model;
+using TestApp.ViewModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,44 @@ namespace TestApp.View.Teacher
     /// </summary>
     public sealed partial class CreateQuestionView : Page
     {
+        Model.Teacher teacherInstance = Model.Teacher.Instance;
+        TeacherCreateViewModel createInstance = TeacherCreateViewModel.Instance;
+
         public CreateQuestionView()
         {
             this.InitializeComponent();
+        }
+
+        private async void CreateQuestionClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!int.TryParse(textBox_QuestionPoints.Text, out int points))
+                    throw new FormatException("Points total invalid. Use numbers only");
+
+                if(comboBox_QuestionType.Text == "Multiple Choice")
+                    createInstance.CreatedQuestion = new Question(0, comboBox_QuestionType.Text, textBox_questionText.Text, textBox_CorrectAnswer.Text,textBox_IncorrectAnswer1.Text, textBox_IncorrectAnswer2.Text, comboBox_CourseNames.Text, points);
+                else if (comboBox_QuestionType.Text == "Text")
+                    createInstance.CreatedQuestion = new Question(0, comboBox_QuestionType.Text, textBox_questionText.Text, null, null, null, comboBox_CourseNames, points);
+                createInstance.CreateQuestion();
+
+            }
+            catch (FormatException exc)
+            {
+                await new MessageDialog(exc.Message).ShowAsync();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(comboBox_QuestionType.Text == "Multiple Choice")
+            {
+                grid_MultipleChoiceAnswers.Visibility = Visibility.Visible;
+            }
+            else if (comboBox_QuestionType.Text == "Text")
+            {
+                grid_MultipleChoiceAnswers.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
