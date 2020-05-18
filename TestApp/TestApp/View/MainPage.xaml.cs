d32c4a9cbebb0f99253a849933fe0500dfe1449d
NDacevic <n.dacevic@gmail.com>
2020-05-18 10:56:21 +0200

    Added navigation functionality to the code-behind

--- TestApp/TestApp/View/MainPage.xaml.cs
+++ TestApp/TestApp/View/MainPage.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TestApp.View;
+using TestApp.View.Student;
+using TestApp.View.Teacher;
+using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +29,28 @@ namespace TestApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_NavigateToPage(object sender, NavigationEventArgs e)
+        {
+            if (sender == bttn_HistoricalTestsViewNavigate)
+                mainFrame.Navigate(typeof(HistoricalTestsView));
+            else if (sender == bttn_MainPageStudentViewNavigate)
+                mainFrame.Navigate(typeof(MainPageStudentView));
+            else if (sender == bttn_WriteTestViewNavigate)
+                mainFrame.Navigate(typeof(WriteTestView));
+            else if (sender == bttn_CreateQuestionViewNavigate)
+                mainFrame.Navigate(typeof(CreateQuestionView));
+            else if (sender == bttn_CreateTestViewNavigate)
+                mainFrame.Navigate(typeof(CreateTestView));
+            else if (sender == bttn_GradeTestViewNavigate)
+                mainFrame.Navigate(typeof(GradeTestView));
+            else if (sender == bttn_MainPageTeacherViewNavigate)
+                mainFrame.Navigate(typeof(MainPageTeacherView));
+            else if (sender == bttn_StudentResultViewNavigate)
+                mainFrame.Navigate(typeof(StudentResultView));
+            else if( sender == bttn_LogInViewNavigate)
+                mainFrame.Navigate(typeof(LogInView));
         }
     }
 }
