3714c43f7515c8ead0ba36dcd236e9f0370902c0
NDacevic <n.dacevic@gmail.com>
2020-05-18 15:18:49 +0200

    Added code to the PostQuestion and PostTest that specifies the object as an application/json string

--- TestApp/TestApp/ApiHelper.cs
+++ TestApp/TestApp/ApiHelper.cs
@@ -31,7 +31,7 @@ namespace TestApp
         #region Constructors
         public ApiHelper()
         {
-            httpClient.BaseAddress = new Uri(@"https://localhost:5000/api/");
+            httpClient.BaseAddress = new Uri(@"https://localhost:5001/api/");
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
@@ -65,10 +65,14 @@ namespace TestApp
         {
             //Convert the object to a json string.
             jsonString = JsonConvert.SerializeObject(test);
+            Debug.WriteLine(test);
 
             //Set this part of the code into a scope so we don't have to worry about it not getting disposed.
             using (HttpContent content = new StringContent(jsonString))
             {
+                //Set the type of content
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
                 //Call the api and send the Json string.
                 HttpResponseMessage response = await httpClient.PostAsync("test", content);
 
@@ -130,12 +134,15 @@ namespace TestApp
             {
                 //Convert the object to a json string.
                 jsonString = JsonConvert.SerializeObject(question);
-
+                Debug.WriteLine(jsonString);
                 //Set this part of the code into a scope so we don't have to worry about it not getting disposed.
                 using (HttpContent content = new StringContent(jsonString))
                 {
+                    //Set the type of content
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
                     //Call the api and send the Json string.
-                    HttpResponseMessage response = await httpClient.PostAsync("question", content);
+                    HttpResponseMessage response = await httpClient.PostAsync("questions", content);
 
                     //Check if it is successfull. In that case display a message telling the user.
                     //Otherwise throw an error and tell the user that the question was not posted.
