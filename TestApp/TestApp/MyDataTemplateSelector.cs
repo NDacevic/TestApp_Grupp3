c17f49d597ad38019cf47f8e820648e9c67e8d9f
mikaelollhage <mikaelollhage@gmail.com>
2020-05-20 16:02:20 +0200

    Timer working, just need to take current time into account

---
+++ TestApp/TestApp/MyDataTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Model;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TestApp
+{
+    public class MyDataTemplateSelector : DataTemplateSelector
+    {
+        //One property per DataTemplate
+        public DataTemplate MultipleChoiceAnswer { get; set; }
+        public DataTemplate TextAnswer { get; set; }
+
+        /// <summary>
+        /// Method that returns the correct DataTemplate according to specific conditions
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (((Question)item).QuestionType == "Flerval")
+            {
+                return MultipleChoiceAnswer;
+            }
+            else
+            {
+                return TextAnswer;
+            }
+        }
+    }
+}
