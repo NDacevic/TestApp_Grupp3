57105481bdb609536a11e01f1cdd9625d41261f6
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2020-05-22 11:34:22 +0200

    Added model role class

---
+++ TestApp/TestApp/Model/Role.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Model
+{
+    public class Role
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public Role (int roleId, string roleName)
+        {
+            RoleId = roleId;
+            RoleName = roleName;
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
