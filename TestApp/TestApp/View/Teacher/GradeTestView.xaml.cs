00ae77bb4d2fd2ad9b53b5a174c6618f97314233
NDacevic <n.dacevic@gmail.com>
2020-05-23 13:28:50 +0200

    Moved code out of the class and into TeacherGradeViewModel.cs

--- TestApp/TestApp/View/Teacher/GradeTestView.xaml.cs
+++ TestApp/TestApp/View/Teacher/GradeTestView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -92,36 +93,14 @@ namespace TestApp.View.Teacher
         }
 
         /// <summary>
-        /// When the user has finished grading the questions they want to grade.
+        /// When the user has finished grading the questions they want to grade,
         /// This method compiles the questions into a list and sends it off to the API for writing to the database
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void FinishGrading(object sender, RoutedEventArgs e)
         {
-            //TODO: Move most of this method to the TeacherGradeViewModel class
-            //TODO: Incomplete method. Waiting on the StudentQuestionAnswer Class to be implemented fully
-            List<StudentQuestionAnswer> gradedQuestions = new List<StudentQuestionAnswer>();
-            Question question;
-
-            foreach (var item in listView_QuestionsForStudentAndTest.Items)
-            {
-                question = (Question)item;
-                var container = listView_QuestionsForStudentAndTest.ContainerFromItem(item);
-                var children = AllChildren(container);
-                foreach (var x in children)
-                {
-                    RadioButton button = (RadioButton)x;
-                    if(button.Name == "radioButton_QuestionCorrect" && button.IsChecked == true)
-                    {
-                        gradedQuestions.Add(new StudentQuestionAnswer(question.Answer, 0) {  }); //TODO: Change this to the normal constructor once Micke has implemented StudentQuestionAnswer fully
-                    }
-                    else if (button.Name == "radioButton_QuestionIncorrect" && button.IsChecked == true)
-                    {
-                        gradedQuestions.Add(new StudentQuestionAnswer(question.Answer, 0) { }); //TODO: Change this to the normal constructor once Micke has implemented StudentQuestionAnswer fully
-                    }
-                }
-            }
+            gradeInstance.FinishGradingTest(listView_QuestionsForStudentAndTest,);
 
             ungradedTests.Clear();
             studentsWithTestList.Clear();
@@ -130,27 +109,6 @@ namespace TestApp.View.Teacher
             scrollViewer_InitialTestList.Visibility = Visibility.Visible;
             scrollViewer_StudentsUngradedTestofType.Visibility = Visibility.Collapsed;
             scrollViewer_QuestionsForStudentAndTest.Visibility = Visibility.Collapsed;
-
-        }
-
-        /// <summary>
-        /// Goes through a UI element and gets all the children of it that are labeled as 'Controlls'
-        /// </summary>
-        /// <param name="parent"></param>
-        /// <returns></returns>
-        public List<Control> AllChildren(DependencyObject parent)
-        {
-
-            //Todo: Move this method to another class
-            var list = new List<Control>();
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-            {
-                var child = VisualTreeHelper.GetChild(parent, i);
-                if (child is Control)
-                    list.Add(child as Control);
-                list.AddRange(AllChildren(child));
-            }
-            return list;
         }
     }
 }
