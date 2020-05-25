fdc8bf6f533a197a1c4c4f7bf2f5407f42e227af
NDacevic <n.dacevic@gmail.com>
2020-05-25 15:57:39 +0200

    Added interface iNotifyPropertyChanged to Person.Cs

--- TestApp/TestApp/Model/Person.cs
+++ TestApp/TestApp/Model/Person.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestApp.Model
 {
-    public abstract class Person
+    public abstract class Person : INotifyPropertyChanged
     {
         #region Constant Fields
         #endregion
 
         #region Fields
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string password;
 
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
 
         #region Constructors
@@ -38,12 +44,12 @@ namespace TestApp.Model
         #endregion
 
         #region Properties
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string FirstName { get => firstName; set { firstName = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName")); } }
+        public string LastName { get => lastName; set { lastName = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName")); } }
+        public string Email { get => email; set { email = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email")); } }
+        public string Password { get => password; set { password = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Password")); } }
 
-        
+
         #endregion
 
         #region Methods
