c537124f607e8313e672e42e0c6b6936fff70cb9
NDacevic <n.dacevic@gmail.com>
2020-05-26 14:42:46 +0200

    Added a NavigationHelper class. This can be used to keep a referense to the Outermost frame so we can tell it to navigate from wherever in the app we are

---
+++ TestApp/TestApp/NavigationHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace TestApp
+{
+    class NavigationHelper
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        private NavigationHelper instance;
+        #endregion
+
+        #region Constructors
+        private NavigationHelper()
+        {
+
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public NavigationHelper Instance { get
+            {
+                if (instance == null)
+                {
+                    instance = new NavigationHelper();
+                }
+                return instance;
+            } 
+        }
+
+        public Frame GlobalFrame { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
