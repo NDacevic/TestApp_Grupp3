9f963226df045d570db81cef5b91c873b924e05b
NDacevic <n.dacevic@gmail.com>
2020-05-27 14:08:28 +0200

    Changed the name of the Main(X)View pages to (X)MenuPage to describe them more accurately

--- TestApp/TestApp/View/Student/StudentMenuPage.xaml.cs
+++ TestApp/TestApp/View/Student/StudentMenuPage.xaml.cs
@@ -20,10 +20,10 @@ namespace TestApp.View.Student
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPageStudentView : Page
+    public sealed partial class StudentMenuPage : Page
     {
         private Frame mainFrame;
-        public MainPageStudentView()
+        public StudentMenuPage()
         {
             this.InitializeComponent();
         }
