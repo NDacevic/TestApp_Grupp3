d1efbcf0944e8828be87ce9003bf8dba1cf903f9
NDacevic <n.dacevic@gmail.com>
2020-05-28 11:36:17 +0200

    Added try-catch to PostTestResult

--- TestApp/TestApp/ApiHelper.cs
+++ TestApp/TestApp/ApiHelper.cs
@@ -289,29 +289,36 @@ namespace TestApp
 
         public async void PostTestResult(TestResult testResult)
         {
-            //Convert the object to a json string.
-            jsonString = JsonConvert.SerializeObject(testResult);
-
-            //Set this part of the code into a scope so we don't have to worry about it not getting disposed.
-            using (HttpContent content = new StringContent(jsonString))
+            try
             {
-                //Set the type of content
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                //Convert the object to a json string.
+                jsonString = JsonConvert.SerializeObject(testResult);
 
-                //Call the api and send the Json string.
-                HttpResponseMessage response = await httpClient.PostAsync("TestResults", content);
+                //Set this part of the code into a scope so we don't have to worry about it not getting disposed.
+                using (HttpContent content = new StringContent(jsonString))
+                {
+                    //Set the type of content
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-                //Check if it is successfull. In that case display a message telling the user.
-                //Otherwise throw an error and tell the user that the question was not posted.
-                if (response.IsSuccessStatusCode)
-                {
-                    await new MessageDialog("Resultatet för testet har sparats").ShowAsync();
+                    //Call the api and send the Json string.
+                    HttpResponseMessage response = await httpClient.PostAsync("TestResults", content);
+
+                    //Check if it is successfull. In that case display a message telling the user.
+                    //Otherwise throw an error and tell the user that the question was not posted.
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await new MessageDialog("Resultatet för testet har sparats").ShowAsync();
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"Http Error: {response.StatusCode}. {response.ReasonPhrase}");
+                        throw new HttpRequestException("Ett fel har uppstått, kontakta administratör");
+                    }
                 }
-                else
-                {
-                    Debug.WriteLine($"Http Error: {response.StatusCode}. {response.ReasonPhrase}");
-                    throw new HttpRequestException("Ett fel har uppstått, kontakta administratör");
-                }
+            }
+            catch (Exception exc)
+            {
+                await new MessageDialog(exc.Message).ShowAsync();
             }
         }
 
