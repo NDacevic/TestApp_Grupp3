2fede2ec0d115c2d8418e84df648b902a3936f5c
NDacevic <n.dacevic@gmail.com>
2020-06-01 12:20:02 +0200

    Added a region around some test code so it doesn't clutter the method.

--- TestApp/TestApp/View/LogInView.xaml.cs
+++ TestApp/TestApp/View/LogInView.xaml.cs
@@ -96,6 +96,7 @@ namespace TestApp.View
         {
             string password = LogInViewModel.EncryptPassword(PB_InsertPassword.Password);
 
+            #region testCode
             //!!Following code is only for testing purpose.
 
             //if(Tb_InsertEmail.Text=="1")
@@ -113,7 +114,7 @@ namespace TestApp.View
             //    this.Frame.Navigate(typeof(MainPage), "Admin");
             //}
             ////TestCode stopped
-
+            #endregion
 
 
             if (Rb_Student.IsChecked == true)
